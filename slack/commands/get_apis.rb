b21461f1e2728cd98c1c46640508536115d44541
vinyasbj <vinyas555@gmail.com>
2020-02-14 18:28:37 +0530

    new command added

--- slack/commands/get_apis.rb
+++ slack/commands/get_apis.rb
@@ -1,12 +1,32 @@
+require "httparty"
 module Slack
     module Commands
         class GetApis < SlackRubyBot::Commands::Base
             command 'get_last_api' do |client, data, _match|
-                client.say(channel: data.channel,text: "this is test api")
+                response = HTTParty.get('https://swagger-apis.herokuapp.com/latest_api')
+                id = response.parsed_response["request"]["id"]
+                title = response.parsed_response["request"]["title"]
+                project = response.parsed_response["request"]["collection_name"]
+                link = "https://api-docs.vinyas.im/try_request/#{id}"
+
+                client.say(channel: data.channel,text: "Latest Api is #{title} from #{project} \n #{link}")
             end
 
             command 'say_hello' do |client, data, _match|
                 client.say(channel: data.channel, text: HelloText.say_hello)
+            end
+
+            command 'get' do |client ,data, _match|
+                response = HTTParty.get("https://swagger-apis.herokuapp.com/search_requests?search=#{_match}")
+                @text = []
+                response.parsed_response["requests"].each do |res|
+                    id = res["id"]
+                    title = res["title"]
+                    project = res["collection_name"]
+                    link = "https://api-docs.vinyas.im/try_request/#{id}"
+                    @text << "Latest Api is #{title} from #{project} \n #{link}"
+                end
+                client.say(channel: data.channel,text: @text.to_sentence )
             end
 
         end
